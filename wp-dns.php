b3833f25c6ac11f63080858f4a6349e373bf771a
combssm <combssm@gmail.com>
2014-09-04 10:56:25 -0400

    Create wp-dns.php

---
+++ wp-dns.php
@@ -0,0 +1,12 @@
+<?php 
+/*  
+  Plugin Name: WP DNS  
+  Plugin URI: http://www.mtbtothemoon.com/wordpress  
+  Description: DNS/whois will be displayed for the site's URL.  
+  Version: 1.0  
+  Author: Sean Combs  
+  Author URI: http://www.mtbtothemoon.com/wordpress
+  License: GPLv3  
+*/
+
+include 'functions.php';
